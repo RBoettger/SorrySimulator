ecc809f69b83ed49b484ab8b9f1ac96cf6b809af
renan.boettger <renan.boettger18@gmail.com>
2025-10-16 00:09:19 -0300

    Configurando EF, criação do banco de dados hospedado

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SorrySimulator.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+string sqlConnectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<SorrySimulatorDbContext>(options =>
+    options.UseSqlServer(sqlConnectionString));
 
 var app = builder.Build();
 
