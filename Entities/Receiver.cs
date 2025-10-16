edc817fe96fff83d3a3b925d26ed70ecd66deb59
boett <boett@DESKTOP-G9MOVVD>
2025-10-15 21:19:03 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Receiver.cs
@@ -0,0 +1,9 @@
+﻿namespace SorrySimulator.Entities
+{
+    public class Receiver
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+    }
+}
